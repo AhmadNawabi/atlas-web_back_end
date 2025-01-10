82e6f0e1c28c2761ed8a6ef9e03936876c39e3d8
AhmadNawabi <ahmadshamoon0505@gmail.com>
2025-01-10 13:20:40 -0500

    updated basic annotations add and remove whitespaces

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
-"""This fuction takes a float 
+"""This fuction takes a float
     a and float b as arguments"""
-def add(a: float , b: float) -> float:
+
+
+def add(a: float, b: float) -> float:
     """And return their sum as a float."""
     return a + b
