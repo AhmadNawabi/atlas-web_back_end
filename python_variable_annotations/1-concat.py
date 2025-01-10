857f8f37747c6100ed419f5de98e613070018b70
AhmadNawabi <ahmadshamoon0505@gmail.com>
2025-01-10 13:27:15 -0500

    Basic concatanation  - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+    This function takes string str1 and string str2
+    as arguments.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returns concataned string"""
+    return str1 + str2
+
+str1 = "egg"
+str2 = "shell"
