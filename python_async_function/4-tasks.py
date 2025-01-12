66bda90748eb1afbff4436cdc48489d7b8f09410
AhmadNawabi <ahmadshamoon0505@gmail.com>
2025-01-11 21:42:41 -0500

    add sorted function

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -7,17 +7,7 @@ task_wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """
-    Spawns `wait_random` `n` times with the specified `max_delay`.
-
-    Args:
-        n (int): The number of times to spawn `wait_random`.
-        max_delay (int): The maximum delay value to pass to `wait_random`.
-
-    Returns:
-        typing.List[float]: A list of all the delays (float values)
-          in ascending order,
-                            without using the `sort()` method.
-    """
+    """Alter wait_n to use task_wait_random"""
     tasks = [task_wait_random(max_delay) for _ in range(n)]
-    return [await task for task in asyncio.as_completed(tasks)]
+    result = [await task for task in asyncio.as_completed(tasks)]
+    return sorted(result)
