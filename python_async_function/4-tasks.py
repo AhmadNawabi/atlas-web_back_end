424d9be5a669d760b16edf408b84a631f23a0b14
AhmadNawabi <ahmadshamoon0505@gmail.com>
2025-01-11 21:36:14 -0500

    updated the return

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """Import asyncio and typing from the library."""
 import asyncio
-import typing
+from typing import List
 """Import wait_random from the previous file."""
 task_wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> typing.List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Spawns `wait_random` `n` times with the specified `max_delay`.
 
@@ -20,10 +20,4 @@ async def task_wait_n(n: int, max_delay: int) -> typing.List[float]:
                             without using the `sort()` method.
     """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
-    results = []
-
-    for task in asyncio.as_completed(tasks):
-        result = await task
-        results.append(result)
-
-    return results
+    return [await task for task in asyncio.as_completed(tasks)]
