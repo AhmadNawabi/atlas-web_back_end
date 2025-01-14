b23646fb3431a0a3d9f230395ce3f39b90f8a58b
AhmadNawabi <ahmadshamoon0505@gmail.com>
2025-01-13 22:10:53 -0500

    updated documentation

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -3,50 +3,68 @@
 from base_caching import BaseCaching
 """
 Create a class BasicCache that inherits from
-BaseCaching and is a caching system:
+BaseCaching and implements a caching system:
 
-You must use self.cache_data - dictionary from
-the parent class BaseCaching
-This caching system doesn’t have limit
-def put(self, key, item):
-Must assign to the dictionary self.cache_data the
-item value for the key key.
-If key or item is None, this method should not
-do anything.
-def get(self, key):
-Must return the value in self.cache_data linked to key.
-If key is None or if the key doesn’t exist in self.cache_data,
-return None.
+This caching system doesn't have a limit.
 """
 
 
 class BasicCache(BaseCaching):
-    """You must use self.cache_data - dictionary
-    from the parent class BaseCaching"""
+    """A basic caching system that uses a dictionary
+    to store cached items.
+
+    Inherits from BaseCaching and uses the `cache_data` dictionary
+    to store cached items. It provides methods to add and retrieve
+    items from the cache."""
+
     def __init__(self):
+        """
+        Initialize the BasicCache instance.
+
+        Initializes the `cache_data` dictionary inherited from the parent class
+        to store the cached items.
+        """
         self.cache_data = {}
 
-    """
     def put(self, key, item):
-    Must assign to the dictionary self.cache_data
-    the item value for the key key.
-    If key or item is None, this method should not do anything.
-    """
+        """
+        Add an item to the cache.
 
-    def put(self, key, item):
+        Assigns the value `item` to the given
+        `key` in the `cache_data` dictionary.
+        If either `key` or `item` is `None`,
+        this method will not add the item
+        to the cache.
+
+        Args:
+            key (str): The key for the cache.
+            item (str): The item to store in the cache.
+
+        Returns:
+            None: This method does not return any value.
+        """
         self.cache_data[key] = item
         if key or item == None:
             pass
         return self.cache_data
 
-    """
     def get(self, key):
-    Must return the value in self.cache_data linked to key.
-    If key is None or if the key doesn’t exist in self.cache_data,
-    return None.
-    """
+        """
+        Retrieve an item from the cache.
 
-    def get(self, key):
+        Retrieves the value associated with
+        `key` from the `cache_data` dictionary.
+        If the `key` is `None` or does not exist
+        in the cache, returns `None`.
+
+        Args:
+            key (str): The key to retrieve from the cache.
+
+        Returns:
+            str or None: The cached item if found,
+            or `None` if the `key` is `None`
+            or not in the cache.
+        """
         if key == None:
             return None
         if key not in self.cache_data:
