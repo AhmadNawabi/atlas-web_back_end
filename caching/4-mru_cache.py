168d747827f17956e82f4bbd02ab2f93ef91d872
AhmadNawabi <ahmadshamoon0505@gmail.com>
2025-01-14 22:36:35 -0500

    updated MRU file

--- caching/4-mru_cache.py
+++ caching/4-mru_cache.py
@@ -49,4 +49,8 @@ class MRUCache(BaseCaching):
         """
         if key is None or key not in self.cache_data:
             return None
+
+        self.order.remove(key)
+        self.order.append(key)
+
         return self.cache_data[key]
