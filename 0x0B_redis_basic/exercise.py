77d570737a2d22964d5277d80a7207b07ffb489e
AhmadNawabi <ahmadshamoon0505@gmail.com>
2025-02-08 13:30:54 -0500

    fix annoted None to str

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -20,7 +20,7 @@ class Cache:
         """
         Store the data in Redis and return a unique key.
         """
-        key = None(uuid.uuid4())
+        key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
